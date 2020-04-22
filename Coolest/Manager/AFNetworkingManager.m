ac0ef7ea5b24122b074c737ad0406c8a41ea8677
lichuanmin <Chuanmin.Li-DAJ@YumChina.com>
2020-04-22 18:43:57 +0800

    AFNetworking update method

--- Coolest/Manager/AFNetworkingManager.m
+++ Coolest/Manager/AFNetworkingManager.m
@@ -64,7 +64,7 @@ static AFHTTPSessionManager *AFNManager = nil;
     [AFNManager.requestSerializer didChangeValueForKey:@"timeoutInterval"];
     [AFNManager.requestSerializer setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
     
-    [AFNManager GET:urlString parameters:params progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+    [AFNManager GET:urlString parameters:params headers: nil progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
         
         NSHTTPURLResponse *urlResponse = (NSHTTPURLResponse *) task.response;
         
@@ -101,45 +101,40 @@ static AFHTTPSessionManager *AFNManager = nil;
     AFNManager = [AFNetworkingManager shareInstance];
     
     //    AFNManager.responseSerializer.acceptableContentTypes = [NSSet setWithObject:@"application/json"];
-    
-    [AFNManager POST:urlString parameters:params progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
-        
-        NSHTTPURLResponse *urlResponse = (NSHTTPURLResponse *) task.response;
-        
-        if (urlResponse.statusCode == 500) {
-            NSDictionary *successDict = responseObject;
-            NSString *errorStr = successDict[@"error"];
-            if (errorStr) {
-                NSError *error = [NSError errorWithDomain:@"httpError" code:500 userInfo:@{NSLocalizedDescriptionKey: errorStr}];
-                //回调错误信息
-                failed?failed(error):nil;
-                
-            } else {
-                NSError *error = [NSError errorWithDomain:@"httpError" code:500 userInfo:@{NSLocalizedDescriptionKey:@"网络请求错误（500）"}];
-                //回调错误信息
-                failed?failed(error):nil;
-                
-            }
-        } else {
-            //回调成功数据
-            if (responseObject) {
-                //                id backDic = [self dictionaryWithJsonData:responseObject];
-                
-                if (responseObject) {
-                    ResponseModel* response = [ResponseModel mj_objectWithKeyValues:responseObject];
-                    
-                    success?success(response):nil;
-                    
-                }else {
-                    NSLog(@"json有错");
-                }
-            }
-        }
-        
+    [AFNManager POST:urlString parameters:params headers:nil progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+         NSHTTPURLResponse *urlResponse = (NSHTTPURLResponse *) task.response;
+               
+               if (urlResponse.statusCode == 500) {
+                   NSDictionary *successDict = responseObject;
+                   NSString *errorStr = successDict[@"error"];
+                   if (errorStr) {
+                       NSError *error = [NSError errorWithDomain:@"httpError" code:500 userInfo:@{NSLocalizedDescriptionKey: errorStr}];
+                       //回调错误信息
+                       failed?failed(error):nil;
+                       
+                   } else {
+                       NSError *error = [NSError errorWithDomain:@"httpError" code:500 userInfo:@{NSLocalizedDescriptionKey:@"网络请求错误（500）"}];
+                       //回调错误信息
+                       failed?failed(error):nil;
+                       
+                   }
+               } else {
+                   //回调成功数据
+                   if (responseObject) {
+                       //                id backDic = [self dictionaryWithJsonData:responseObject];
+                       
+                       if (responseObject) {
+                           ResponseModel* response = [ResponseModel mj_objectWithKeyValues:responseObject];
+                           
+                           success?success(response):nil;
+                           
+                       }else {
+                           NSLog(@"json有错");
+                       }
+                   }
+               }
     } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
-        
         failed?failed(error):nil;
-        
     }];
 }
 
